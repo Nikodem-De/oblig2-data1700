bd22d868d206239741a6ddf73dc039bc1069d58c
Nikodem-De <niden7144@oslomet.no>
2026-03-06 18:00:42 +0100

    task1.js

---
+++ task1.js
@@ -0,0 +1,25 @@
+// Task 1: Converting HTML data to JS
+const student = {
+  studentId: "S-123456",
+  name: {
+    first: "John",
+    last: "Doe",
+    preferred: "Joe",
+  },
+  age: 21,
+  contact: {
+    email: "john.doe@example.edu",
+    phone: {
+      mobile: "+47 234 45 678",
+      home: "(not provided)"
+    },
+      address: {
+            street: "Pilestredet 46",
+            city: "Oslo",
+            postalCode: "0167",
+            country: "Norway"
+        }
+    }
+};
+
+console.log("Task 1 - Student Object:", student);
